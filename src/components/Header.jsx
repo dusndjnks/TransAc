86d28029ba39964e590a7564f4a3701c0f053387
dusndjnks <mhdaffan0000@gmail.com>
2025-07-19 23:19:20 +0530

    x

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -81,7 +81,7 @@ const Header = () => {
       )}
 
       {/* Main Navigation */}
-      <div className="max-w-7xl mx-auto px-4 ">
+      <div className="max-w-7xl mx-auto pr-4 ">
         <div className="flex justify-between items-center h-16 ">
           {/* Logo */}
         <NavLink to="/" className="flex items-center ">
